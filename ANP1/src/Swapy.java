8fcfbe1d920e4455390ee49165ded4b75be7828b
Swapnali <Swapnali@LAPTOP-KK9BA8K2>
2022-10-25 17:00:51 +0530

    version2 new line added

---
+++ ANP1/src/Swapy.java
@@ -0,0 +1,10 @@
+
+public class Swapy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("swapnali");
+System.out.println("pokharkar");
+	}
+
+}
