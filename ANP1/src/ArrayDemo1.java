aba02a881f8f5b515622a8e28cd89f5ffe499cc2
Swapnali <Swapnali@LAPTOP-KK9BA8K2>
2022-10-26 14:26:09 +0530

    version3 updated

---
+++ ANP1/src/ArrayDemo1.java
@@ -0,0 +1,22 @@
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e;
+		a=90;
+		b=5;
+		c=34;
+		d=67;
+		e=34;
+		int arr1[]= {90,5,34,67,34};//single dimensional array
+		                            //declaring and initializing the array
+		int arr2[]=new int[2];//declaring single dimensional array with size 2
+		arr2[0]=12; //initialization
+		arr2[1]=78;
+		//System.out.println(arr2[1]);
+		for(int i=0;i<arr1.length;i++)
+			System.out.println(arr1[1]);
+		
+	}
+
+}
